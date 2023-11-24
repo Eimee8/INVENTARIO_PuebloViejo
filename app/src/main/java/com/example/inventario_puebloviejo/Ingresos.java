1a61c40df9dfea8b55cfac0a006b6ac4a9842e5e
Mhugo716 <mhugo1873@gmail.com>
2023-11-23 20:10:44 -0600

    -Eliminacion de fragments -Creacion de Activitys para la navegacion -Creacion de metodo de onClick para la funcion de las pantallas

--- app/src/main/java/com/example/inventario_puebloviejo/Ingresos.java
+++ app/src/main/java/com/example/inventario_puebloviejo/Ingresos.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Egresos extends AppCompatActivity {
+public class Ingresos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_egresos);
+        setContentView(R.layout.activity_ingresos);
     }
 }
--- app/src/main/java/com/example/inventario_puebloviejo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/inventario_puebloviejo/ui/home/HomeFragment.java
@@ -5,21 +5,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.inventario_puebloviejo.R;
-import com.example.inventario_puebloviejo.databinding.FragmentHomeBinding;
+import com.example.inventario_puebloviejo.Cajas;
+import com.example.inventario_puebloviejo.Catastro;
+import com.example.inventario_puebloviejo.Contraloria;
 import com.example.inventario_puebloviejo.Egresos;
+import com.example.inventario_puebloviejo.Ingresos;
+import com.example.inventario_puebloviejo.R;
+import com.example.inventario_puebloviejo.Registro_Civil;
+import com.example.inventario_puebloviejo.Secretarias;
+import com.example.inventario_puebloviejo.Tesoreria;
+import com.example.inventario_puebloviejo.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
 
-    ImageView egresos;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,17 +35,83 @@ public class HomeFragment extends Fragment {
 
         View root = binding.getRoot();
 
-       egresos = (ImageView) root.findViewById(R.id.egresos);
+        ImageView egresosView = root.findViewById(R.id.egresos);
+        ImageView ingresosView = root.findViewById(R.id.ingresos);
+        ImageView tesoreriaView = root.findViewById(R.id.tesoreria);
+        ImageView cajasView = root.findViewById(R.id.cajas);
+        ImageView catastroView = root.findViewById(R.id.catastro);
+        ImageView contraloriaView = root.findViewById(R.id.contraloria);
+        ImageView registroView = root.findViewById(R.id.registrocivil);
+        ImageView secretariasView = root.findViewById(R.id.secretarias);
 
 
-       egresos.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent intent = new Intent(getActivity(), Egresos.class);
-               startActivity(intent);
-           }
-       });
+        egresosView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent intent = new Intent(getContext(), Egresos.class);
+                startActivity(intent);
+
+            }
+        });
+
+        ingresosView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getContext(), Ingresos.class);
+                startActivity(intent);
+
+            }
+        });
+
+        tesoreriaView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Tesoreria.class);
+                startActivity(intent);
+            }
+        });
+
+        cajasView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Cajas.class);
+                startActivity(intent);
+            }
+        });
+
+        catastroView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Catastro.class);
+                startActivity(intent);
+            }
+        });
+
+        contraloriaView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Contraloria.class);
+                startActivity(intent);
+            }
+        });
+
+        registroView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Registro_Civil.class);
+                startActivity(intent);
+            }
+        });
+
+        secretariasView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Secretarias.class);
+                startActivity(intent);
+            }
+        });
 
 
         return root;
