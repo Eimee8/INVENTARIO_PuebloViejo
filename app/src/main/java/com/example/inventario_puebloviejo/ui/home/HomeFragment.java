a61fa8d2d011ecb4b9ae10d877d0d36cc232642a
Mhugo716 <mhugo1873@gmail.com>
2023-11-25 17:57:56 -0600

    -Creacion de Activitys para la navegacion -Creacion de metodo de onClick para la funcion de las pantallas

--- app/src/main/java/com/example/inventario_puebloviejo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/inventario_puebloviejo/ui/home/HomeFragment.java
@@ -12,13 +12,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.inventario_puebloviejo.A_Tecnologia;
+import com.example.inventario_puebloviejo.Alcoholes;
 import com.example.inventario_puebloviejo.Cajas;
 import com.example.inventario_puebloviejo.Catastro;
+import com.example.inventario_puebloviejo.Comandancia;
+import com.example.inventario_puebloviejo.Comercio;
 import com.example.inventario_puebloviejo.Contraloria;
+import com.example.inventario_puebloviejo.D_Social;
 import com.example.inventario_puebloviejo.Egresos;
+import com.example.inventario_puebloviejo.Informatica;
 import com.example.inventario_puebloviejo.Ingresos;
 import com.example.inventario_puebloviejo.Inst_Mujer;
 import com.example.inventario_puebloviejo.Obra_Publica;
+import com.example.inventario_puebloviejo.Panteones;
 import com.example.inventario_puebloviejo.R;
 import com.example.inventario_puebloviejo.Regidores;
 import com.example.inventario_puebloviejo.Registro_Civil;
@@ -26,6 +32,8 @@ import com.example.inventario_puebloviejo.Religion;
 import com.example.inventario_puebloviejo.Rh;
 import com.example.inventario_puebloviejo.Secretarias;
 import com.example.inventario_puebloviejo.Tesoreria;
+import com.example.inventario_puebloviejo.Trabajo_Social;
+import com.example.inventario_puebloviejo.Transparencia;
 import com.example.inventario_puebloviejo.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -55,6 +63,14 @@ public class HomeFragment extends Fragment {
         ImageView RHView = root.findViewById(R.id.rrhh);
         ImageView religionView = root.findViewById(R.id.religion);
         ImageView institutoMView = root.findViewById(R.id.mujer);
+        ImageView comandanciaView = root.findViewById(R.id.comandancia);
+        ImageView comercioView = root.findViewById(R.id.comercio);
+        ImageView alcoholView = root.findViewById(R.id.alcoholes);
+        ImageView panteonesView = root.findViewById(R.id.panteones);
+        ImageView transparenciaView = root.findViewById(R.id.transparencia);
+        ImageView drllo_socialView = root.findViewById(R.id.desarrollosocial);
+        ImageView informaticalView = root.findViewById(R.id.informatica);
+        ImageView t_socialView = root.findViewById(R.id.trabajosocial);
 
 
         egresosView.setOnClickListener(new View.OnClickListener() {
@@ -169,6 +185,70 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), Inst_Mujer.class);
+                startActivity(intent);
+            }
+        });
+
+        comandanciaView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Comandancia.class);
+                startActivity(intent);
+            }
+        });
+
+        comercioView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Comercio.class);
+                startActivity(intent);
+            }
+        });
+
+        alcoholView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Alcoholes.class);
+                startActivity(intent);
+            }
+        });
+
+        panteonesView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Panteones.class);
+                startActivity(intent);
+            }
+        });
+
+        transparenciaView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Transparencia.class);
+                startActivity(intent);
+            }
+        });
+
+        drllo_socialView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), D_Social.class);
+                startActivity(intent);
+            }
+        });
+
+        informaticalView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Informatica.class);
+                startActivity(intent);
+            }
+        });
+
+        t_socialView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Trabajo_Social.class);
                 startActivity(intent);
             }
         });
