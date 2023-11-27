11e8d103babb9ec7f9208ea3414dbb6fd1855e9f
Eimee8 <151586546+Eimee8@users.noreply.github.com>
2023-11-26 19:30:36 -0600

    cambio de diseño de gallery creacion de registro

---
+++ app/src/main/java/com/example/inventario_puebloviejo/Registro_equipo.java
@@ -0,0 +1,14 @@
+package com.example.inventario_puebloviejo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_equipo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_equipo);
+    }
+}
