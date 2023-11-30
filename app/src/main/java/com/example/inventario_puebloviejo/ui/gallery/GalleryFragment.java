1f7d142cd7f571c1c17b2e28af9ec56b3470b354
Mhugo716 <mhugo1873@gmail.com>
2023-11-29 21:28:44 -0600

    -Funcion de insertar en tabla equipo -Calendario en registro_equipo

--- app/src/main/java/com/example/inventario_puebloviejo/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/inventario_puebloviejo/ui/gallery/GalleryFragment.java
@@ -1,10 +1,12 @@
 package com.example.inventario_puebloviejo.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +14,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.inventario_puebloviejo.R;
+import com.example.inventario_puebloviejo.Registro_equipo;
 import com.example.inventario_puebloviejo.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
+    Button registro;
     private FragmentGalleryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -26,6 +30,15 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        registro = (Button) root.findViewById(R.id.btnRegistro);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Registro_equipo.class);
+                startActivity(intent);
+            }
+        });
 
 
         return root;
