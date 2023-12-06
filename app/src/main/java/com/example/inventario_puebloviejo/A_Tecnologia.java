e6234c6b4571110a6e9180b236b70e2cfbc6ac36
Mhugo716 <120601361+Mhugo716@users.noreply.github.com>
2023-12-06 01:05:40 -0600

    -Creacion de boton de descarga PDF -Implementacion de libreria para PDF -Implementacion de permisos para PDF -Creacion de funcion de descarga de PDF

--- app/src/main/java/com/example/inventario_puebloviejo/A_Tecnologia.java
+++ app/src/main/java/com/example/inventario_puebloviejo/A_Tecnologia.java
@@ -1,8 +1,16 @@
 package com.example.inventario_puebloviejo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
 
@@ -10,10 +18,26 @@ import com.example.inventario_puebloviejo.db.AdapterEquipo;
 import com.example.inventario_puebloviejo.db.DataBase;
 import com.example.inventario_puebloviejo.db.Date;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
+
+import android.os.Environment;
+import android.widget.Toast;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class A_Tecnologia extends AppCompatActivity {
 
+    private static final int REQUEST_PERMISSIONS = 1;
+    private static final int REQUEST_CODE_CREATE_PDF = 123; // Puedes usar cualquier valor único aquí
     DataBase db;
     private ArrayList<Date> date;
     RecyclerView recyclerView;
@@ -32,5 +56,68 @@ public class A_Tecnologia extends AppCompatActivity {
 
         AdapterEquipo adapter = new AdapterEquipo(date, this);
         recyclerView.setAdapter(adapter);
+
+        Button generarPDFbtn = findViewById(R.id.btnPDF);
+
+        generarPDFbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generarPDF();
+            }
+        });
     }
+    private void generarPDF() {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+        intent.putExtra(Intent.EXTRA_TITLE, "equipos.pdf");
+
+        startActivityForResult(intent, REQUEST_CODE_CREATE_PDF);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_CREATE_PDF && resultCode == RESULT_OK) {
+            if (data != null && data.getData() != null) {
+                Uri uri = data.getData();
+                savePDFToUri(uri);
+            }
+        }
+    }
+
+    private void savePDFToUri(Uri uri) {
+        try {
+            OutputStream outputStream = getContentResolver().openOutputStream(uri);
+            if (outputStream != null) {
+                // Configurar el escritor PDF
+                PdfWriter pdfWriter = new PdfWriter(outputStream);
+                PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+
+                // Inicializar el documento iTextPdf
+                Document document = new Document(pdfDocument);
+
+                // Obtener datos de la tabla y agregarlos al documento
+                for (Date equipo : date) {
+                    document.add(new Paragraph("Estatus: " + equipo.getEstatus()));
+                    document.add(new Paragraph("Marca: " + equipo.getMarca()));
+                    document.add(new Paragraph("Número de Serie: " + equipo.getN_serie()));
+                    document.add(new Paragraph("Nombre de Área: " + equipo.getNombre_area()));
+                    document.add(new Paragraph("Fecha de Inicio: " + equipo.getFecha_ini()));
+                    document.add(new Paragraph("Propietario: " + equipo.getPropietario()));
+                    document.add(new Paragraph("----------------------------------------"));
+                }
+
+                document.close();
+                outputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
